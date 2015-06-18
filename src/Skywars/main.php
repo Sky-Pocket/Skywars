4e11f9d7f45181441e2293083b53cd9de3dfff1a
robozeri <robozerirobot@gmail.com>
2015-06-18 09:19:03 +0200

    Update main.php

--- src/Skywars/main.php
+++ src/Skywars/main.php
@@ -12,36 +12,70 @@ use pocketmine\scheduler\Task;
 
 use SkyWars\MiniGameBase;
 use SkyWars\SkyWarsCommand;
-@author robozeriandsvile
+
 class Main extends PluginBase implements CommandExecutor
 {
- public $commands;
- public $worldManager;
- public $gameKit;
- public $setup;
- public $controller; 
- public $profileprovider;
- public $playArenas = [];
- 
- private $arena1;
- private $arena2;
- private $arena3;
- private $arena4;
- private $arena5;
- 
-  public function onLoad() 
-  {
-        $this->commands = new SkyWarsCommand ( $this );
-	$this->worldManager = new SkyWarsWorldManager ( $this );
-	$this->gameKit = new SkyWarsGameKit($this);
-	$this->setup = new SKyWarsSetup($this);
-	$this->profileprovider = new SKyWarsProvider ( $this );
+ 	public $commands;
+	public $arenaManager;
+	public $gameKit;
+	public $setup;
+	public $controller;
+	public $profileprovider;
+	public $defenceManager;
+	public $blocks;
+	public $pos_display_flag;
+	public $playArenas = [];	
+	public $backupArenas = [];
 	
-  }
-  public function onEnable
-  {
-  		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-        	$this->saveDefaultConfig();
-            	$this->points = new Config($this->getDataFolder()."arena1.yml", Config::YAML);
+	public $redTeam = [];
+	public $blueTeam = [];
+
+	public $arrowShooter = [];
 	
-  }
+	public $setupModeAction = "";
+	public $setupModeData = "";
+	public $homeLevel = null;
+	public $gameMode = 0;	
+	public $forceReset = true;
+	
+	public $npcs = [];
+	public $npcsPositions = [];
+	public $npcsPodium = [];
+	public $statueManager;
+	public $npcsSpawns = [];
+
+	public $playerParticles = [];
+	
+	public function getHomeLevel() {
+		return $this->homeLevel;
+	}
+	public function setHomeLevel($hlevel) {
+		$this->homeLevel = $hlevel;
+	}
+	public function onLoad() {
+		$this->commands = new SkyWarsCommand ( $this );
+		$this->arenaManager = new ArenaManager ( $this );
+		$this->gameKit = new SkyWarsGameKit($this);
+		$this->setup = new SkyWarsSetup($this);
+		$this->controller = new SkyWarsController($this);
+		$this->profileprovider = new ProfileProvider ( $this );
+	}
+	public function onEnable() {
+		$this->initConfigFile ();
+		$this->enabled = true;
+		$this->getServer ()->getPluginManager ()->registerEvents ( new SkyWarsListener ( $this ), $this );
+		
+		$this->arenaManager->loadArenas ();
+		
+		$this->statueManager = new StatueManager( $this );
+		$this->statueManager->loadStatues ();
+		
+		if ($this->profileprovider != null) {
+			$this->profileprovider->initlize ();
+		}
+		
+		
+		$this->initScheduler();
+		
+		$this->log ( TextFormat::GREEN . "- [SkyWars] has been enabled -!" );
+	}
